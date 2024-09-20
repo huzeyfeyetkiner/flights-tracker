1fc97f668ac564daf1e4e31342deb83253d99e8d
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-09-21 00:19:45 +0300

    FlightsResponse type implemented to api

--- src/api/fetch-flights.ts
+++ src/api/fetch-flights.ts
@@ -1,9 +1,11 @@
+import { FlightsResponse } from "@/types/fligths-types"
+
 const apiKey = "f81ae60e65879d2f5bbd9fb7ba61be5b"
 
 const apiUrl =
 	"https://api.schiphol.nl/public-flights/flights?includedelays=false&page=0&sort=%2BscheduleTime"
 
-export const fetchFlights = async () => {
+export const fetchFlights = async (): Promise<FlightsResponse | undefined> => {
 	try {
 		const response = await fetch(apiUrl, {
 			method: "GET",
@@ -21,6 +23,8 @@ export const fetchFlights = async () => {
 		}
 
 		const data = await response.json()
+		console.log(data)
+
 		return data
 	} catch (error) {
 		console.log(error)
